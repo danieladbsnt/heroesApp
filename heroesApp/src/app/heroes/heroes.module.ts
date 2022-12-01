ccdd4ac89a7637b63639bea60095b3bc0388ecee
danieladbsnt <daniela.darnea@bosonit.com>
2022-12-01 18:43:44 +0100

    added imports

--- heroesApp/src/app/heroes/heroes.module.ts
+++ heroesApp/src/app/heroes/heroes.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { HeroeCardComponent } from './components/heroe-card/heroe-card.component';
 
 
 
@@ -15,11 +19,13 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     SearchComponent,
     HeroeComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    HeroeCardComponent
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
