a082bcfb475d7b0840b299897e4f0acbeae359db
danieladbsnt <daniela.darnea@bosonit.com>
2022-12-02 14:22:36 +0100

    added router and getBack method

--- heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
 })
 export class HeroeComponent implements OnInit {
+heroe!: Heroe;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private heroesService: HeroesService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(switchMap(({id}) => this.heroesService.getHeroeById(id)))
+    .subscribe(heroe => this.heroe = heroe)    
+  }
+
+  getBack(){
+    this.router.navigate(['/heroes/list'])
   }
 
 }
