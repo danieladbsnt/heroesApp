7aa15cacf2c898bb7d87f38aa15d1d19300b7d61
danieladbsnt <daniela.darnea@bosonit.com>
2022-12-12 12:16:57 +0100

    added selectedOption function and selectedHeroe

--- heroesApp/src/app/heroes/pages/search/search.component.ts
+++ heroesApp/src/app/heroes/pages/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -10,13 +11,27 @@ export class SearchComponent implements OnInit {
   termino: string = '';
   heroes: Heroe[] = [];
 
+  selectedHeroe: Heroe | undefined;
+
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
 
   searching() {
-    this.heroesService.getHeroes()
+    this.heroesService.getSuggestion(this.termino.trim())
     .subscribe(heroes => this.heroes = heroes)
+  }
+
+  selectedOption(event: MatAutocompleteSelectedEvent) {
+    if (!event.option.value) {
+      this.selectedHeroe = undefined;
+      return;
+      
+    }
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+    this.heroesService.getHeroeById(heroe.id!) 
+    .subscribe(heroe => this.selectedHeroe = heroe);
   }
 }
